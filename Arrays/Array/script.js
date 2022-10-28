e52756514fa7aad6988a694a9a06a332fa6dfa52
Sergio Sala <cf21sergio.sala@iesjoandaustria.org>
2022-10-28 23:44:36 +0200

    Falta función media y ordenar por peso, nombre y retoques estéticos para mejora

--- Arrays/Array/script.js
+++ Arrays/Array/script.js
@@ -11,9 +11,9 @@ let borderColor =[];
 let resultadoBusqueda=[];
 let sumaPesos = 0;
 /* let Objectpokemons = {id,
-				nombre,
-				img,
-				height}; */ 
+                nombre,
+                img,
+                height}; */ 
 
 
 // POKEMONS
@@ -21,131 +21,151 @@ let sumaPesos = 0;
 fetch("data/pokemon.json")
 .then((response) => response.json())
 .then((data) => {
-	dades = data.pokemon;
+    dades = data.pokemon;
 
-	dades.forEach(pokemon=>{
-		let pok = [pokemon.id, pokemon.name, pokemon.height,pokemon.img];
-		/* let pokemonsObject = {} */
+    dades.forEach(pokemon=>{
+        let pok = [pokemon.id, pokemon.name, pokemon.height,pokemon.img];
+        arrayPokemons.push(pok);
+        //He creado un array de 3 dimensiones.
+        pokemon.type.forEach(type=>{
+            if(!arrayLabels.includes(type)){
+                arrayLabels.push(type);
+                arrayDadesGraf.push(0);
+                //Cada vez q pasa por aquí pushea un 0
+            }
 
-		arrayPokemons.push(pok);
-
-		pokemon.type.forEach(type=>{
-			if(!arrayLabels.includes(type)){
-				arrayLabels.push(type);
-				arrayDadesGraf.push(0);
-				//Cada vez q pasa por aquí pushea un 0
-			}
-
-			//Buscar l'índex
-			let index = arrayLabels.indexOf(type);
-			arrayDadesGraf[index]++;
-		})
-	})
-	
-	console.table(arrayDadesGraf); 
-	printList(arrayPokemons);
+            //Buscar l'índex
+            let index = arrayLabels.indexOf(type);
+            arrayDadesGraf[index]++;
+        })
+    })
+   
+    //console.table(arrayDadesGraf);//luego este me interesa
+    printList(dades);
 });
 
 //FUNCIONES
 
 //reiniciar
 function refresh(){
-	location.reload(); 
+    location.reload();
 }
 //mostrar lista pokemons
 function printList(llista){
-	contenidoTabla.innerHTML="";
-	llista.forEach(pokemon => {
-		let fila = document.createElement("tr");
-		//creo fila
-		let tdId = document.createElement("td");
-		//creo elemento tdid 
-		tdId.innerHTML= pokemon[0];
-		/**pongo contenido a la variable que he creado */
-		fila.appendChild(tdId);
-		/**Le digo donde va , en este caso le digo q es hijo de esta fila*/
-		let tdName = document.createElement("td");
-		tdName.innerHTML = pokemon[1];
-		fila.appendChild(tdName);
+    contenidoTabla.innerHTML="";
+    llista.forEach(pokemon => {
+        let fila = document.createElement("tr");
+        //creo fila
+        let tdId = document.createElement("td");
+        //creo elemento tdid
+        tdId.innerHTML= pokemon.id;
+        /**pongo contenido a la variable que he creado */
+        fila.appendChild(tdId);
+        /**Le digo donde va , en este caso le digo q es hijo de esta fila*/
+        let tdName = document.createElement("td");
+        tdName.innerHTML = pokemon.name;
+        fila.appendChild(tdName);
 
-		let tdWeaknesses = document.createElement("td"); 
-		tdWeaknesses.innerHTML = pokemon[2];
-		fila.appendChild(tdWeaknesses);
-		
-		let tdImage = document.createElement("td");
-		tdImage.innerHTML = `<img src="${pokemon[3]}"/>`;
-		fila.appendChild(tdImage);
-		tdImage.classList.add("flex");
-		tdImage.classList.add("justify-center");
+        let tdWeaknesses = document.createElement("td");
+        tdWeaknesses.innerHTML = pokemon.height;
+        fila.appendChild(tdWeaknesses);
+       
+        let tdImage = document.createElement("td");
+        tdImage.innerHTML = `<img src="${pokemon.img}"/>`;
+        fila.appendChild(tdImage);
+        tdImage.classList.add("flex");
+        tdImage.classList.add("justify-center");
 
-		
-		contenidoTabla.appendChild(fila);
-		/*la fila está dentro de contenido tabla. Cojo toda la fila y la pongo dentro de contenido tabla, para seguir con la siguiente */
-	});
+       
+        contenidoTabla.appendChild(fila);
+        /*la fila está dentro de contenido tabla. Cojo toda la fila y la pongo dentro de contenido tabla, para seguir con la siguiente */
+    });
 }
 
 //ordenar
 function orderList(tipoOrden){
-	if(tipoOrden === "Desc"){
-		console.table(arrayPokemons.sort().reverse());
-	}
 
-	if(tipoOrden === "Asce"){//No está ordenado,preguntar.
-		console.table(arrayPokemons.reverse().sort());
-	}
+   
+    if(tipoOrden === "Desc"){
 
-	printList(arrayPokemons);
+        dades.reverse();
+        console.log(dades);
+       
+    }
+
+    if(tipoOrden === "Asce"){//No está ordenado,preguntar.
+       
+        dades.reverse();
+        console.log(dades);
+       
+    }
+
+    printList(dades);
 }
+
+
 
 //Calcula media
 function calcMitjana(){//en ejemplo calcula media de peso
-	//toFixed(2)//para que coja sólo 2 decimales
-	arrayPokemons.forEach()
+    //toFixed(2)//para que coja sólo 2 decimales
+    
+	for(let i = 0; i < arrayPokemons.length;i ++){
+		sumaPesos = sumaPesos + arrayPokemons[2];
+
+	}
+	console.log(sumaPesos);
+
 }
 
 //Mostrar gráfico
 function showGraph(){
-	//document.getElementById("chart-container").style.display("");//me falta ponerle algo dentro. LUego tengo que hacer lo mismo con otro para que empiece estando escondido
+    //document.getElementById("chart-container").style.display("");//me falta ponerle algo dentro. LUego tengo que hacer lo mismo con otro para que empiece estando escondido
+   
+    let buscaTable = document.getElementById("buscaTable");
+    buscaTable.classList.add("hidden");
+    arrayLabels.forEach(nuevoColor=>{
+        let r = Math.random()*256;
+        let g = Math.random()*256;
+        let b = Math.random()*256;
+        backgroundColor.push("rgba("+r+","+g+","+b+",0.3)");
+        borderColor.push("rgb("+r+","+g+","+b+")");
+    });
 
-	arrayLabels.forEach(nuevoColor=>{
-		let r = Math.random()*256;
-		let g = Math.random()*256;
-		let b = Math.random()*256; 
-		backgroundColor.push("rgba("+r+","+g+","+b+",0.3)");
-		borderColor.push("rgb("+r+","+g+","+b+")");
-	});
+    const data = {
+        labels: arrayLabels,
+        datasets: [{
+            label: 'My First Dataset',
+            data: arrayDadesGraf,
+            backgroundColor: [
+           
+            ],
+            borderColor: borderColor,
+            backgroundColor: backgroundColor,
+        }]
+    };
 
-	const data = {
-		labels: arrayLabels,
-		datasets: [{
-			label: 'My First Dataset',
-			data: arrayDadesGraf,
-			backgroundColor: [
-			
-			],
-			borderColor: borderColor,
-			backgroundColor: backgroundColor,
-		}]
-	};
+    const config = {
+        type: 'polarArea',
+        data: data,
+        options: {}
+      };
 
-	const config = {
-		type: 'polarArea',
-		data: data,
-		options: {}
-	  };
-
-	  const myChart = new Chart(
-		document.getElementById('myChart'),
-		config
-	  );
+      const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+      );
 }
 
-//Buscador	
+function showTable(){
+	location.reload();
+}
+
+//Buscador  
 let inputSearch = document.getElementById('txtSearch');
 inputSearch.addEventListener('input', (e) => {
-	let arraySearch = arrayPokemons.filter(function(element){
-		return element[1].toLowerCase().includes(inputSearch.value.toLowerCase());
-	});
-	console.log(arraySearch);
-	printList(arraySearch)
+    let arraySearch = arrayPokemons.filter(function(element){
+        return element[1].toLowerCase().includes(inputSearch.value.toLowerCase());
+    });
+    console.log(arraySearch);
+    printList(arraySearch)
 }); 
